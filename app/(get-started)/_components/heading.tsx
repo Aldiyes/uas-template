2a4d7ae629d56abf1d8cc95f215be739639e8a95
aldiyespaskalisbirta <aldiyespaskalisbirta@gmail.com>
2024-06-06 06:33:45 +0700

    feat(auht): create card wrapper for wrap form

--- app/(get-started)/_components/heading.tsx
+++ app/(get-started)/_components/heading.tsx
@@ -2,6 +2,7 @@
 
 import { MdKeyboardDoubleArrowRight } from 'react-icons/md';
 
+import { LoginButton } from '@/components/auth/login-button';
 import { Button } from '@/components/ui/button';
 
 export const Heading = () => {
@@ -11,13 +12,19 @@ export const Heading = () => {
 				Do More of the Things that Matter Most. Welcome to{' '}
 				<span className="underline">NextTodos</span>
 			</h1>
+
 			<h3 className="font text-base font-medium sm:text-xl md:text-2xl">
 				NextTodos help you develop positive habits and achieve goals
 			</h3>
-			<Button>
-				Enter NextTodos
-				<MdKeyboardDoubleArrowRight className="ml-2 h-4 w-4" />
-			</Button>
+			
+			<div>
+				<LoginButton>
+					<Button>
+						Enter NextTodos
+						<MdKeyboardDoubleArrowRight className="ml-2 h-4 w-4" />
+					</Button>
+				</LoginButton>
+			</div>
 		</div>
 	);
 };
---
+++ app/auth/layout.tsx
@@ -0,0 +1,11 @@
+export default function AuthLayout({
+	children,
+}: {
+	children: React.ReactNode;
+}) {
+	return (
+		<div className="h-full flex items-center justify-center dark:bg-[#1F1F1F]">
+			{children}
+		</div>
+	);
+}
