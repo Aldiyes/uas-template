2af6d622245cd70ba50650afbfc8aa151bbedc82
aldiyespaskalisbirta <aldiyespaskalisbirta@gmail.com>
2024-06-06 05:34:07 +0700

    feat: create get started page

---
+++ app/(get-started)/_components/heading.tsx
@@ -0,0 +1,23 @@
+'use client';
+
+import { MdKeyboardDoubleArrowRight } from 'react-icons/md';
+
+import { Button } from '@/components/ui/button';
+
+export const Heading = () => {
+	return (
+		<div className="max-w-3xl space-y-4">
+			<h1 className="text-3xl font-bold sm:text-5xl md:text-6xl">
+				Do More of the Things that Matter Most. Welcome to{' '}
+				<span className="underline">NextTodos</span>
+			</h1>
+			<h3 className="font text-base font-medium sm:text-xl md:text-2xl">
+				NextTodos help you develop positive habits and achieve goals
+			</h3>
+			<Button>
+				Enter NextTodos
+				<MdKeyboardDoubleArrowRight className="ml-2 h-4 w-4" />
+			</Button>
+		</div>
+	);
+};
