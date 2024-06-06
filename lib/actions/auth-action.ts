17b65d436157c044e5774d7896ad3d893209aaf6
aldiyespaskalisbirta <aldiyespaskalisbirta@gmail.com>
2024-06-06 09:26:47 +0700

    feat(auth): create register form

--- lib/actions/auth-action.ts
+++ lib/actions/auth-action.ts
@@ -2,10 +2,21 @@
 
 import * as z from "zod";
 
-import { LoginSchema } from "@/lib/schemas";
+import { LoginSchema, RegisterSchema } from "@/lib/schemas";
 
 export const login = async (values: z.infer<typeof LoginSchema>) => {
   const validatedFields = LoginSchema.safeParse(values);
+
+  if (!validatedFields.success) {
+    return { error: "Invalid fields!" };
+  }
+  return {
+    success: "Email sent!",
+  };
+};
+
+export const register = async (values: z.infer<typeof RegisterSchema>) => {
+  const validatedFields = RegisterSchema.safeParse(values);
 
   if (!validatedFields.success) {
     return { error: "Invalid fields!" };
