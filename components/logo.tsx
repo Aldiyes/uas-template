3550844a99a7a2415913adca177eb39b6cda570a
aldiyespaskalisbirta <aldiyespaskalisbirta@gmail.com>
2024-06-06 09:37:17 +0700

    fix: waring aspec ration for next/image

--- components/logo.tsx
+++ components/logo.tsx
@@ -10,17 +10,19 @@ export const Logo = () => {
     <div className="hidden items-center gap-x-2 md:flex">
       <Image
         src="/logo.svg"
-        height={20}
-        width={20}
+        height="20"
+        width="20"
         alt="Logo"
         className="dark:hidden"
+        priority
       />
       <Image
         src="/logo-dark.svg"
-        height={20}
-        width={20}
+        height="20"
+        width="20"
         alt="Logo"
         className="hidden dark:block"
+        priority
       />
       <p className={cn("font-semibold", poppins.className)}>NextTodos</p>
     </div>
