377b0fa93ce432b7077d452c070ce0d91d07b6f1
aldiyespaskalisbirta <aldiyespaskalisbirta@gmail.com>
2024-06-06 08:21:13 +0700

    feat: create get started page

---
+++ components/logo.tsx
@@ -0,0 +1,15 @@
+import { Poppins } from "next/font/google";
+import Image from "next/image";
+
+import { cn } from "@/lib/utils";
+
+const poppins = Poppins({ subsets: ["latin"], weight: ["400", "600"] });
+
+export const Logo = () => {
+  return (
+    <div className="hidden items-center gap-x-2 md:flex">
+      <Image src="/logo.svg" height={20} width={20} alt="Logo" />
+      <p className={cn("font-semibold", poppins.className)}>NextTodos</p>
+    </div>
+  );
+};
