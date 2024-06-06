85ac12ec14bcb161befa103e8fa5c802c5e0e560
aldiyespaskalisbirta <aldiyespaskalisbirta@gmail.com>
2024-06-06 07:33:42 +0700

    feat(auth): create register form

--- lib/schemas.ts
+++ lib/schemas.ts
@@ -10,8 +10,13 @@ export const LoginSchema = z.object({
 });
 
 export const RegisterSchema = z.object({
-	firstName: z.string(),
-	lastName: z.string(),
-	email: z.string().email(),
-	password: z.string(),
+	name: z.string().min(1, {
+		message: 'Name is required',
+	}),
+	email: z.string().email({
+		message: 'Email is required',
+	}),
+	password: z.string().min(6, {
+		message: 'Password must contain at least 6 characters',
+	}),
 });
